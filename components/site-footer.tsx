66f8ad45e49a888bbb2419db06bd5dbdd4c68b28
Sam Melius Oxford <samsjmelius@gmail.com>
2026-08-14 15:58:29 -0700

    added candid cert in footer

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -5,7 +5,18 @@ export function SiteFooter() {
   return (
     <footer className="site-footer">
       <div className="shell footer-grid">
-        <div className="footer-brand"><Image src="/homegrown4heroes-logo.png" alt="Homegrown 4 Heroes" width={150} height={150} /><p>Helping veterans heal, grow, and build community through accessible plant-based wellness education.</p></div>
+        <div className="footer-brand">
+          <Image src="/homegrown4heroes-logo.png" alt="Homegrown 4 Heroes" width={150} height={150} />
+          <p>Helping veterans heal, grow, and build community through accessible plant-based wellness education.</p>
+        </div>
+        <div className="footer-candid">
+          <Image
+            src="/2026_candid.png"
+            alt="Candid Platinum Transparency 2026"
+            width={190}
+            height={190}
+          />
+        </div>
         <div><h3>Explore</h3><Link href="/about">About</Link><Link href="/programs">Programs</Link><Link href="/partners">Partners</Link><Link href="/the-happy-veteran">The Happy Veteran</Link></div>
         <div><h3>Get Involved</h3><Link href="/get-involved/sponsorship">Sponsorship</Link><Link href="/get-involved/2026-endorsements">2026 Endorsements</Link><a href="https://calendly.com/homegrown4heroes/new-member-consult" target="_blank" rel="noreferrer">Schedule a consult</a><a href="mailto:hello@homegrown4heroes.org">hello@homegrown4heroes.org</a></div>
       </div>
