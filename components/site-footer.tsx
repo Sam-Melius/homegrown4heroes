14e0c2b7146c15d3af85d554c434ae4f84e39e5a
Sam Melius Oxford <samsjmelius@gmail.com>
2026-08-17 08:57:29 -0700

    added Safe access to footer

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -18,7 +18,14 @@ export function SiteFooter() {
           />
         </div>
         <div><h3>Explore</h3><Link href="/about">About</Link><Link href="/programs">Programs</Link><Link href="/partners">Partners</Link><Link href="/the-happy-veteran">The Happy Veteran</Link></div>
-        <div><h3>Get Involved</h3><Link href="/get-involved/sponsorship">Sponsorship</Link><Link href="/get-involved/2026-endorsements">2026 Endorsements</Link><a href="https://calendly.com/homegrown4heroes/new-member-consult" target="_blank" rel="noreferrer">Schedule a consult</a><a href="mailto:hello@homegrown4heroes.org">hello@homegrown4heroes.org</a></div>
+        <div><h3>Get Involved</h3><Link href="/get-involved/sponsorship">Sponsorship</Link><Link href="/get-involved/2026-endorsements">2026 Endorsements</Link><a href="https://calendly.com/homegrown4heroes/new-member-consult" target="_blank" rel="noreferrer">Schedule a consult</a><a href="mailto:hello@homegrown4heroes.org">hello@homegrown4heroes.org</a>
+        <a
+          href="https://www.safeaccessnow.org/states#gsc.tab=0"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Laws In My State
+        </a></div>
       </div>
       <div className="shell footer-bottom">© 2026 Homegrown4Heroes · 501(c)(3)</div>
     </footer>
